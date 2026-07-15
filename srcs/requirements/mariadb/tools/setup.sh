46463b4ddeebdc8214ee0566d58abe0911e40a0d
Bluvern <zealvern6@gmail.com>
2026-07-15 16:49:51 +0200

    Changed mariadb Dockerfile and setup

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,16 +5,20 @@ set -e
 echo "Starting MariaDB setup..."
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB..."
+    echo "Initializing database..."
 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 	mysqld_safe &
 	until mariadb-admin ping >/dev/null 2>&1; do
     	sleep 1
 	done
-	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-	mysql -e "FLUSH PRIVILEGES;"
+	echo "Running SQL..."
+	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+	mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+	mariadb -u root -e "FLUSH PRIVILEGES;"
+	echo "Stopping temporary server..."
+	mariadb-admin shutdown
 fi
 
+echo "Starting MariaDB..."
 exec mysqld_safe
