9ea3e0182040cb7fe88d92a67f8ed0527c17bcf6
Bluvern <zealvern6@gmail.com>
2026-07-21 10:30:32 +0200

    Added NGINX

---
+++ srcs/requirements/nginx/tools/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting NGINX..."
+
+mkdir -p /etc/nginx/ssl
+
+if [ ! -f /etc/nginx/ssl/inception.crt ] || \
+   [ ! -f /etc/nginx/ssl/inception.key ]; then
+
+    echo "Generating SSL certificate..."
+
+    openssl req -x509 \
+        -nodes \
+        -days 365 \
+        -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/inception.key \
+        -out /etc/nginx/ssl/inception.crt \
+        -subj "/C=ES/ST=Catalonia/L=Barcelona/O=42/OU=Student/CN=manualva.42.fr"
+fi
+
+echo "Starting NGINX..."
+
+exec nginx -g "daemon off;"
