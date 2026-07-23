c360510071d44dec2344a080036f2cd06aaaa3af
Bluvern <zealvern6@gmail.com>
2026-07-23 11:03:42 +0200

    Changed setup.sh for new containers

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -10,31 +10,38 @@ MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing database..."
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-    mysqld_safe &
-    for i in $(seq 1 30); do
-        if mariadb-admin ping >/dev/null 2>&1; then
-            break
-        fi
-        sleep 1
-    done
-    echo "Running SQL..."
-	mariadb -u root -e \
-		"ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-	mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
-		"CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
-		"CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
-		"GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-	mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
-		"FLUSH PRIVILEGES;"
-	echo "Stopping temporary server..."
-	mariadb-admin \
-		-u root \
-		-p"$MYSQL_ROOT_PASSWORD" \
-		shutdown
 fi
 
-echo "MariaDB setup complete!"
 echo "Starting MariaDB..."
-exec mysqld_safe
+mysqld_safe &
+echo "Waiting for MariaDB..."
+
+for i in $(seq 1 30); do
+    if mariadb-admin ping >/dev/null 2>&1; then
+        break
+    fi
+    sleep 1
+done
+
+if ! mariadb-admin ping >/dev/null 2>&1; then
+    echo "MariaDB failed to start."
+    exit 1
+fi
+
+echo "MariaDB is ready."
+
+echo "Configuring database..."
+mariadb -u root -e \
+    "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" 2>/dev/null || true
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
+    "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
+    "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
+    "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -e \
+    "FLUSH PRIVILEGES;"
+
+echo "MariaDB setup complete!"
+
+wait
