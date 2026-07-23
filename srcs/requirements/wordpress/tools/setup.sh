ecd9d951d54da39549a8351dc66d52ba1346ecdd
Bluvern <zealvern6@gmail.com>
2026-07-23 10:16:42 +0200

    More password adjustments

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -5,6 +5,8 @@ set -e
 echo "Starting WordPress..."
 
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
 mkdir -p /run/php
 
